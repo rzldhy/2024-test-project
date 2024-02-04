c263d8a5a7e1d24333bfaaae7b84745ce0d8c596
rizal28 <rizaldhyrsmd@gmail.com>
2024-02-04 23:31:36 +0700

    day 3-hero.js

---
+++ 3-hero.js
@@ -0,0 +1,18 @@
+// 3/366
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+// function hero(bullets, dragons){
+//   if(bullets / 2 >= dragons) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function hero(bullets, dragons){
+//   return bullets / 2 >= dragons;
+// }
+
+const hero = (bullets, dragons) => bullets / 2 >= dragons;
+
+console.log(hero(10, 5));
