edf3ed2987608c2181bcd61c8b90125af670a7a2
rizal28 <rizaldhyrsmd@gmail.com>
2024-02-04 23:10:02 +0700

    day 2/366

---
+++ reverseSeq.js
@@ -0,0 +1,14 @@
+// 2/366
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d/solutions/javascript
+
+// const reverseSeq = n => {
+//     const result = [];
+//   for(let i = n; i >= 1; i--) {
+//     result.push(i);
+//   }
+//     return result;
+//   };
+
+const reverseSeq = (n) => [...Array(n)].map((el, i) => n - i);
+
+console.log(reverseSeq(5));
