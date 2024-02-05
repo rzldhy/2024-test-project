2c8a32f00b837c45185df74551e3ca8b8edff6c7
rizal28 <rizaldhyrsmd@gmail.com>
2024-02-05 09:30:22 +0700

    day 4/366

---
+++ 4-smash.js
@@ -0,0 +1,16 @@
+// function smash (words) {
+//     return words.join(' ');
+// }
+
+// const smash = (words) => words.join(' ');
+
+function smash (words) {
+    let result = '';
+    for(let i = 0; i < words.length; i++) {
+        result += words[i];
+        if(i != words.length - 1) result += ' ';
+    }
+    return result;
+}
+
+ console.log(smash(['this', 'is', 'a', 'really', 'long', 'sentence']));
