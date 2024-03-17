695dffdc6e380c84f0946f86befd9b405133ae3e
rizal28 <rizaldhyrsmd@gmail.com>
2024-03-18 05:58:37 +0700

    day 22/366

---
+++ 22-removeExclamationMarks.js
@@ -0,0 +1,20 @@
+// 22/366
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+
+// function removeExclamationMarks(s) {
+//   let result = '';
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] !== "!") {
+//       result += s[i];
+//     }
+//   }
+//   return result;
+// }
+
+// function removeExclamationMarks(s) {
+//   return s.split('!').join('');
+// }
+
+const removeExclamationMarks = (s) => s.split("!").join("");
+
+console.log(removeExclamationMarks("Hello World!"));
