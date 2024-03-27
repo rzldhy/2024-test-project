15b603e9e06d5d8777a2a97ac2cfd9fe1ea3c9db
rizal28 <rizaldhyrsmd@gmail.com>
2024-03-28 00:51:10 +0700

    day 25/366

---
+++ 25-doublechar.js
@@ -0,0 +1,26 @@
+// 25/366
+// https://www.codewars.com/kata/56b1f01c247c01db92000076/train/javascript
+
+// function doubleChar(str) {
+//     let result = '';
+//     for (let i = 0; i < str.length; i++) {
+//         result += str[i];
+//         result += str[i];
+//     }
+//     return result;
+// }
+
+// function doubleChar(str) {
+//   return str
+//     .split("")
+//     .map((el) => el + el)
+//     .join("");
+// }
+
+const doubleChar = (str) =>
+  str
+    .split("")
+    .map((el) => el + el)
+    .join("");
+
+console.log(doubleChar("Adidas"));
