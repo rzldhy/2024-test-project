8f5ca7760bb5608cda5d58d8aac0a8ccf9469d4d
rizal28 <rizaldhyrsmd@gmail.com>
2024-04-09 12:33:39 +0700

    27/366

---
+++ 27-setalarm.js
@@ -0,0 +1,32 @@
+// 27/366
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/javascript
+
+// function setAlarm(employed, vacation) {
+//   if (employed) {
+//     if (vacation) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   } else {
+//     if (vacation) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+// }
+
+// function setAlarm(employed, vacation) {
+//   return employed && !vacation;
+// }
+
+const setAlarm = (employed, vacation) => employed && !vacation;
+
+console.log(setAlarm(true, true));
+
+// employed | vacation
+// true     | true     => false
+// true     | false    => true
+// false    | true     => false
+// false    | false    => false
