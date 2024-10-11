3a71298caf98e0dfebb615c7da045587e3891327
rizal28 <rizaldhyrsmd@gmail.com>
2024-10-11 20:49:17 +0700

    day 31/366

---
+++ 31-Third Angle of a Triangle.js
@@ -0,0 +1,10 @@
+// 31/366
+// https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+
+// function otherAngle(a, b) {
+//   return 180 - (a + b);
+// }
+
+const otherAngle = (a, b) => 180 - (a + b);
+
+console.log(otherAngle(30, 60));
