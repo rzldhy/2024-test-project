37fff58e1fbc7ba77b614c7df9751eb47beb5c53
rizal28 <rizaldhyrsmd@gmail.com>
2025-09-06 11:06:31 +0700

    fix

---
+++ test27.js
@@ -0,0 +1,28 @@
+//www.codewars.com/kata/513fa1d75e4297ba38000003/train/javascript
+
+https: function flatten(...args) {
+  const result = [];
+
+  function helper(item) {
+    if (Array.isArray(item)) {
+      for (let el of item) {
+        helper(el);
+      }
+    } else {
+      result.push(item);
+    }
+  }
+
+  for (let arg of args) {
+    helper(arg);
+  }
+
+  return result;
+}
+
+// Examples
+console.log(flatten(1, [2, 3], 4, 5, [6, [7]]));
+// [1, 2, 3, 4, 5, 6, 7]
+
+console.log(flatten("a", ["b", 2], 3, 666, [[4], ["c"]]));
+// ['a', 'b', 2, 3, 666, 4, 'c']
